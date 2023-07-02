afb00584eb9ab6526588a7509b8849887c594640
lazulyTech <ksuke.ropt@gmail.com>
2023-07-02 23:37:17 +0900

    New theme

--- script.js
+++
@@ -1,7 +0,0 @@
-function lastModifiedDay() {
-	var day = new Date(document.lastModified);
-	var y = day.getFullYear();
-	var m = day.getMonth() + 1;
-	var d = day.getDate();
-	document.write("(最終更新 " + y + "/" + m + "/" + d + ")");
-}
